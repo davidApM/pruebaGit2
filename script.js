9ef5c2022ef0a185bacaee907e25dfe74e970a37
David_A <pm13119@ues.edu.sv>
2024-05-04 15:54:18 -0600

    Modifique la funciones de js

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ agregarBtn.addEventListener('click', function() {
         alert('No se ha agregado ningún elemento.');
     }
 });
+
+var eliminarBtn = document.getElementById('eliminarBtn'); 
+
+eliminarBtn.addEventListener('click', function(){
+    // Mostrar una ventana de confirmación
+     var confirmacion = confirm('¿Estás seguro de que quieres eliminar un elemento?');
+    
+     // Verificar si el usuario confirmó
+     if (confirmacion) {
+         // Aquí puedes agregar la lógica para agregar un elemento a la tabla
+         alert('Elemento eliminado correctamente.');
+     } else {
+         alert('No se ha eliminado ningún elemento.');
+     }
+
+})
